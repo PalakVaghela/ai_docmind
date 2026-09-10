528169f5f6d51e28d0d467dfdbbf19b5b9e672bd
PalakVaghela <palakvaghela83@gmail.com>
2026-09-10 17:38:24 +0530

    [IMP] Connected ollama with relevent chunks and make sure it gives ans from given document context

--- llm.py
+++ llm.py
@@ -1,4 +1,5 @@
 from langchain_ollama import ChatOllama
+from retriever import retrive_document
 
 MODEL_NAME = "llama3.2"
 
@@ -13,8 +14,39 @@ def call_llm(prompt: str):
     response = llm.invoke(prompt)
     return response.content
 
+def answer_question(query: str):
+    documents = retrive_document(query)
+    context = "\n\n".join(
+        document.page_content for document in documents
+    )
+    prompt = f"""
+    You are a helpful document assistant.
+
+    Answer the user's question using the provided context.
+
+    If the answer cannot be found in the context, say:
+    "I couldn't find the answer in the provided documents."
+
+    Context:
+    {context}
+
+    Question:
+    {query}
+
+    Answer:
+    """
+    return call_llm(prompt)
+
 if __name__ == "__main__":
-    prompt = "Explain what FastAPI is in simple words."
+    # prompt = "In which column in gernal ledger we have to add?"
+    prompt = "Who is best friend of doraemon in japanees cartoon doraemon?"
     response = call_llm(prompt)
-    print("\nOllama response:")
+    print("\nOllama response=========================================")
     print(response)
+
+    ans = answer_question(prompt)
+    print("\nAnswer:================================================")
+    print(ans)
+
+# currenlty the llm is independent it is not taking context of document now we have to add that.
+# and currenlty the we are only getting relevant chunks only.
